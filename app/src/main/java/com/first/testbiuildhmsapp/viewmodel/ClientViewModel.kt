5230292b74b9829449fbb393ab8da0e6b68c8b7f
Вовчик <sizikoffps@gmail.com>
2024-10-03 16:00:54 +0300

    Настроен OperatorH

--- app/src/main/java/com/first/testbiuildhmsapp/viewmodel/ClientViewModel.kt
+++ app/src/main/java/com/first/testbiuildhmsapp/viewmodel/ClientViewModel.kt
@@ -17,8 +17,8 @@ class ClientViewModel : ViewModel() {
     init {
         // Инициализируем состояние с данными клиента
         _state.value = ClientState(
-            clientName = "OperatorA",
-            textColor = 0xFF5733  // Задаем цвет для OperatorA
+            clientName = "OperatorH",
+            textColor = 0x8C33FF // Задаем цвет для OperatorA
         )
     }
 
